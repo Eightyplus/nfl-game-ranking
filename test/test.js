89126b15a2407eba80160380f503e15badc43539
Morten Friesgaard <morten.friesgaard@soprasteria.com>
2016-11-23 12:31:46 +0100

    added test for fantasy scores

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
-const assert = require('chai').assert;
+let mocha = require('mocha');
+let chai = require('chai');
+const fs = require('fs');
+
+const describe = mocha.describe;
+const it = mocha.it;
+const assert = chai.assert;
 const util = require('../util');
 
 describe('Fetching stats', function() {
@@ -15,6 +21,55 @@ describe('Fetching stats', function() {
   });
 });
 
+
+describe('Fantasy stats', function() {
+
+  const matches = [
+    ['MIA', 'CIN'],
+    ['TB', 'DEN'],
+    ['MIN', 'NYG'],
+    ['JAX', 'IND'],
+    ['BAL', 'OAK'],
+    ['CHI', 'DET'],
+    ['ATL', 'CAR'],
+    ['WAS', 'CLE'],
+    ['HOU', 'TEN'],
+    ['ARI', 'LA'],
+    ['SD', 'NO'],
+    ['SF', 'DAL'],
+    ['PIT', 'KC'],
+    ['NYJ', 'SEA'],
+    ['NE', 'BUF']
+    // GB PHI
+  ];
+
+  describe('group stats for players', function() {
+    it('Should be able to group into 5 players', function(done) {
+
+      this.timeout(10000);
+      callback = (matchups) => {
+        assert.typeOf(matchups, 'array', 'Calculations returned in array');
+        assert.lengthOf(matchups, 15, 'Expect 15 matches this week');
+
+        done()
+      };
+
+      // skip this test, to slow!
+      //util.calcWeek(4, matches, callback);
+
+      fs.readFile('./test/stats_week4.json', function(error, str) {
+        const data = JSON.parse(str)['players'];
+        const stats = util.calcTeamStats(data);
+        const matchups = util.calcMatchups(stats, matches);
+
+        assert.lengthOf(data, 1354, 'Number of players with fantasy stats');
+        assert.lengthOf(stats, 30, 'Expect 30 teams this week');
+
+        callback(matchups);
+      });
+    });
+  });
+});
 
 describe('Mapping team', function() {
   describe('mapTeam', function() {
