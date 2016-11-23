5e42f10e99119b1f97943590a0338252f4cb8b4f
Morten Friesgaard <morten.friesgaard@soprasteria.com>
2016-11-23 14:50:28 +0100

    updated with use of const and let

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var http = require('http');
-var path = require('path');
-var express = require('express');
-var app = express();
-var util = require('./util');
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+const util = require('./util');
 
 app.get('/ranking', util.calcRanking);
 
 app.use(express.static(__dirname + '/index'));
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 8000, process.env.IP || '127.0.0.1', function(){
-  var addr = server.address();
+  let addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
 
