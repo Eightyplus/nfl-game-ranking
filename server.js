53c7d925277a5a06853677afdac5ac935896a3a3
Morten Friesgaard <friesgaard@gmail.com>
2017-05-18 12:54:19 +0200

    fixes server script for Docker

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const express = require('express');
 const app = express();
 const util = require('./src/server/util');
 
+const port = process.env.PORT || 8000;
+
 app.get('/ranking', util.calcRanking);
+app.get('/health', function(req, res) {
+    res.send('OK');
+});
 
 app.use(express.static(__dirname + '/index'));
 
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 8000, process.env.IP || '127.0.0.1', function(){
-  let addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+app.listen(port, function(){
+    console.log('Server listening on port:' + port);
 });
-
